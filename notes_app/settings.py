3444840ec6bc4bf9661907a851855e645c467fe8
edwardrand <52064142+tanmay-kali@users.noreply.github.com>
2022-09-27 22:48:14 -0500

    added whitenoise

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
