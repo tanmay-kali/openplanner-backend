60222d2c74caf8b5f4d8d3e43f691904314df9c3
edwardrand <52064142+tanmay-kali@users.noreply.github.com>
2022-09-28 09:28:46 -0500

    procFile update

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
+web: gunicorn notes_app.wsgi
 release: python manage.py makemigrations
 release: python manage.py migrate
-web: gunicorn notes_app.wsgi
--- api/models.py
+++ api/models.py
@@ -9,3 +9,6 @@ class Note(models.Model):
 class Test(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
+class tanmaytest(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
